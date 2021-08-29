70c46be504262608e533ce486c584f3671fd0689
Matt Deeds <mdeeds@gmail.com>
2021-08-28 21:31:42 -0700

    Allow adjusting start and end of clip.

--- ts/clip.ts
+++ ts/clip.ts
@@ -4,13 +4,28 @@ export class Clip {
   private buffer: AudioBuffer;
   private audioCtx: AudioContext;
   private audioNode: AudioBufferSourceNode = null;
+  private div: HTMLDivElement;
 
   constructor(
     audioContext: AudioContext,
     buffer: AudioBuffer, startOffsetS: number, durationS: number) {
+    this.audioCtx = audioContext;
     this.startOffsetS = startOffsetS;
     this.durationS = durationS;
     this.buffer = buffer;
+
+    this.div = document.createElement('div');
+    this.div.innerText = 'clip';
+    this.div.classList.add('clip');
+    this.div.tabIndex = 1;
+    document.getElementsByTagName('body')[0].appendChild(this.div);
+    this.div.addEventListener('keydown', (ev: KeyboardEvent) => {
+      switch (ev.code) {
+        case 'ArrowRight': this.changeStart(0.01); break;
+        case 'ArrowLeft': this.changeStart(-0.01); break;
+        case 'Space': this.start(this.audioCtx.currentTime); break;
+      }
+    });
   }
 
   public start(startTimeS: number) {
@@ -23,8 +38,9 @@ export class Clip {
     this.audioNode.buffer = this.buffer;
     this.audioNode.loop = true;
     this.audioNode.loopStart = this.startOffsetS;
+    this.audioNode.loopEnd = this.startOffsetS + this.durationS;
     this.audioNode.connect(this.audioCtx.destination);
-    this.audioNode.start(startTimeS, this.startOffsetS, this.durationS);
+    this.audioNode.start(startTimeS, this.startOffsetS);
   }
 
   public changeStart(deltaS: number) {
